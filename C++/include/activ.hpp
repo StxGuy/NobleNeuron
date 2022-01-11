bad17e04fad33706b4243a61b0baf65599592dd3
Prof. Carlo R. da Cunha, Ph.D <carlo.requiao@gmail.com>
2022-01-11 17:31:31 -0300

    Create activ.hpp

---
+++ C++/include/activ.hpp
@@ -0,0 +1,14 @@
+#ifndef ACTIV_NN
+#define ACTIV_NN
+
+#include <armadillo>
+#include <string>
+
+using namespace std;
+using namespace arma;
+
+
+mat activate(mat, string);
+mat deactivate(mat, string);
+
+#endif
