2896db69877ca28d04b4b9293485bc3974f8b3e5
Prof. Carlo R. da Cunha, Ph.D <carlo.requiao@gmail.com>
2022-01-28 17:53:09 -0300

    Update activ.cpp

--- C++/source/activ.cpp
+++ C++/source/activ.cpp
@@ -16,12 +16,30 @@ mat activate(mat Y,string activation) {
     else
         Z = Y;
     
-    
     return Z;
 }
 
 /* Derivative of the activation function
- * dZdY = (d/dY)sigma(Y) */
+ * dZdY = (d/dY).sigma(Y) */
+cube deactivate(cube Z, string activation) {
+    cube dZdY;
+    
+    transform(activation.begin(),activation.end(),activation.begin(),:: tolower);
+    
+    
+    if (activation == "relu")
+        dZdY = Z.transform([](float val) {return val > 0 ? 1:0;});
+    else if (activation == "sigmoid")
+        dZdY = Z%(1.0 - Z); 
+    else if (activation == "tanh")
+        dZdY = 1.0 - Z%Z;
+    else
+        dZdY = Z;
+    
+    return dZdY;
+}
+
+// For feedforward
 mat deactivate(mat Z, string activation) {
     mat dZdY;
     
