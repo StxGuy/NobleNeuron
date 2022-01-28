225124f12aaf35db995b81611ef7031a98aa084d
Prof. Carlo R. da Cunha, Ph.D <carlo.requiao@gmail.com>
2022-01-28 20:02:20 -0300

    Create activ.hpp

---
+++ C++/include/NobleNeuron_headers/activ.hpp
@@ -0,0 +1,15 @@
+#ifndef ACTIV_NN
+#define ACTIV_NN
+
+#include <armadillo>
+#include <string>
+
+using namespace std;
+using namespace arma;
+
+
+mat  activate(mat, string);
+cube deactivate(cube, string);
+mat  deactivate(mat, string);
+
+#endif
